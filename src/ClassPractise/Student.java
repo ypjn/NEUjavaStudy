013aa0b82909e753317dae648183e6f88463813a
ypjn <lyh169347@163.com>
2026-03-19 11:12:48 +0800

    更新 Student、Test、OJTest 类；新增 vcs.xml 配置文件

--- src/ClassPractise/Student.java
+++ src/ClassPractise/Student.java
@@ -1,17 +1,45 @@
 package ClassPractise;
 
 public class Student {
-    String name;
-    String ID;
-    String major;
+    private String name;
+    private String ID;
+    private String major;
     double GPA;
-    String setmajor(String newMajor){
+     public String setmajor(String newMajor){
         major = newMajor;
         System.out.println("major is set to " + major);
         return major;
     }
-    double setGPA(double GPA){
+    public double setGPA(double GPA){
         System.out.println("GPA is set to " + GPA);
+        return GPA;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public double getGPA() {
         return GPA;
     }
 }
--- src/ClassPractise/Test.java
+++ src/ClassPractise/Test.java
@@ -6,6 +6,7 @@ public class Test {
     public static void main(String[] args) {
         Student s = new Student();
         Scanner sc = new Scanner(System.in);
+        String name = s.getName();
 
     }
 }
